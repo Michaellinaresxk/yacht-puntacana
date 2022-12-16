0b4284339d7f95981d67ef50a7672ad86ef015c1
Michael Linares <116801315+Michaellinaresxk@users.noreply.github.com>
2022-12-16 16:03:13 +0100

    Create main title to home page (#12)

---
+++ components/MainTitle.jsx
@@ -0,0 +1,26 @@
+import { Typography } from "@mui/material";
+
+
+export const MainTitle = () => {
+	return (
+		<>
+			<Typography variant="span"> Somos: </Typography>
+            <Typography  variant="h1"> Yacht Punta Cana </Typography>
+            <Typography  variant="p"> Disponemos de una amplia selección de alquiler de yates, super yates, lanchas y catamaranes únicos, en Punta Cana y Casa de campo.</Typography>
+		</>
+	);
+};
+
+
+
+
+{/* <div class="yacht-info mt-5">
+<div class="banner-info__text">
+    <span data-i18n="weAre.subtitle">Somos:</span>
+    <h1 class="text-center">Yacht Punta Cana</h1>
+    <p class="text-center" data-i18n="weAre.slogan">
+        Disponemos de una amplia selección de alquiler de yates, super yates, lanchas y catamaranes únicos, en
+        Punta Cana y Casa de campo.
+    </p>
+</div>
+</div> */}
