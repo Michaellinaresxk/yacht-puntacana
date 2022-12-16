cfebad25122c7dfe73ab151d120d0abb78ace85a
Michael Linares <michaellinaresDBildungs@gmail.com>
2022-12-16 16:01:34 +0100

    Create main title to home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,6 +6,7 @@ import { theme } from "../styles/theme";
 import { Footer } from "../components/Footer";
 import { Contact } from "../components/Contact";
 import { WhatsAppButton } from "../components/buttons/WhatsAppButton";
+import { MainTitle } from "../components/MainTitle";
 
 export default function Home() {
 	return (
@@ -15,9 +16,11 @@ export default function Home() {
 					<title>Yachts Punta Cana</title>
 					<meta name="description" content="Are you planning to rent a yacht in punta cana? Enjoy a private yacht charter and sail in the Caribbean Sea. We provide the best yacht experience with our luxury yachts. With us you will find the best offers for your unforgettable trip" />
 					<link rel="icon" href="/favicon.ico" />
+					<meta name="google-site-verification" content="XvBRJKvRQn33_vzkKZlM4dEP2f0NyUc6wRa02xEJdFU" />
 				</Head>
 				<DrawerAppBar />
 				<Banner />
+				<MainTitle />
 				<Contact />
 				<Footer />
 				<WhatsAppButton />
