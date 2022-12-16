950be40d8d9be6d059f600289377df56cffad51d
Michael Linares <michaellinaresDBildungs@gmail.com>
2022-12-16 23:27:54 +0100

    Created services hero section

--- pages/services.jsx
+++ pages/services.jsx
@@ -1,11 +1,11 @@
 import DrawerAppBar from "../components/Header/DrawerAppBar";
-import { Typography } from "@mui/material";
+import { ServicesHero } from "../components/hero/ServicesHero";
 
 function Services() {
 	return (
 		<>
 			<DrawerAppBar />
-			<Typography variant="h2">This is the services page</Typography>
+			<ServicesHero />
 		</>
 	);
 }
