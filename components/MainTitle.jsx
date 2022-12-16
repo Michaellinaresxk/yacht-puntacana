dc9c8dd64b116439c47530b50f70de554aa71fcb
AJMS1118 <ajquesadac90@gmail.com>
2022-12-16 13:28:22 -0500

    adding main title component

--- components/MainTitle.jsx
+++ components/MainTitle.jsx
@@ -1,20 +1,38 @@
-import { Typography } from "@mui/material";
-
+import { Typography, Box } from "@mui/material";
 
 export const MainTitle = () => {
 	return (
 		<>
-			<Typography variant="span"> Somos: </Typography>
-            <Typography  variant="h1"> Yacht Punta Cana </Typography>
-            <Typography  variant="p"> Disponemos de una amplia selección de alquiler de yates, super yates, lanchas y catamaranes únicos, en Punta Cana y Casa de campo.</Typography>
+			<Box
+				sx={{
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+					justifyContent: "center",
+					marginTop: "150px",
+					marginBottom: "100px",
+				}}
+			>
+				<Typography variant="subtitle1"> Somos: </Typography>
+				<Typography
+					variant="h2"
+					sx={{ fontWeight: "bold", color: "rgba(0, 0, 0, 0.8)" }}
+				>
+					Yacht Punta Cana
+				</Typography>
+				<Typography variant="h6">
+					Disponemos de una amplia selección de alquiler de yates, super yates,
+				</Typography>
+				<Typography variant="h6">
+					lanchas y catamaranes únicos, en Punta Cana y Casa de campo.
+				</Typography>
+			</Box>
 		</>
 	);
 };
 
-
-
-
-{/* <div class="yacht-info mt-5">
+{
+	/* <div class="yacht-info mt-5">
 <div class="banner-info__text">
     <span data-i18n="weAre.subtitle">Somos:</span>
     <h1 class="text-center">Yacht Punta Cana</h1>
@@ -23,4 +41,5 @@ export const MainTitle = () => {
         Punta Cana y Casa de campo.
     </p>
 </div>
-</div> */}
+</div> */
+}
