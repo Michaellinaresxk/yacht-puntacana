5bfe9b302218069e011f253a7942f3b51fd164d9
Michael Linares <116801315+Michaellinaresxk@users.noreply.github.com>
2022-12-17 00:25:15 +0100

    Added destination hero (#16)

--- components/hero/ServicesHero.jsx
+++ components/hero/ServicesHero.jsx
@@ -1,17 +1,13 @@
 import { Paper } from "@mui/material";
 
-
-
 export const ServicesHero = () => {
 	return (
 		<>
            <Paper
                 sx={{
-                    backgroundImage:
-                        "url(https://res.cloudinary.com/freelancer2222222222222222/image/upload/v1671124919/services_oif68t.png)",
-                    width: "100wv",
-                    maxWidth: "100%",
-                    height: "100%",
+                    backgroundImage: "url(https://res.cloudinary.com/freelancer2222222222222222/image/upload/v1671124919/services_oif68t.png)",
+                    width: "100%",
+                    height: "700",
                     backgroundPosition: "center",
                     backgroundSize: "cover",
                     backgroundRepeat: "no-repeat"
--- pages/destiny.jsx
+++ pages/destiny.jsx
@@ -1,11 +1,11 @@
 import DrawerAppBar from "../components/Header/DrawerAppBar";
-import { Typography } from "@mui/material";
+import { DestinationHero } from "../components/hero/DestinationHero";
 
 function destiny() {
 	return (
 		<>
 			<DrawerAppBar />
-			<Typography variant="h2">This is the destiny page</Typography>
+			<DestinationHero />
 		</>
 	);
 }
