8b05bcf8c93f35ea17c2d0efb6c54daa5c815027
Andrés de Jesús Quesada Cedeño <92536363+AJMS1118@users.noreply.github.com>
2022-12-17 05:14:54 -0500

    adding the new banner component and fixing the hero images (#18)
    Co-authored-by: AJMS1118 <ajquesadac90@gmail.com>

---
+++ components/BannerImageClip.jsx
@@ -0,0 +1,118 @@
+import { Typography, Paper, Box, List } from "@mui/material";
+import { Check } from "@mui/icons-material";
+import { Colors } from "../styles/theme";
+
+export const BannerImageClip = () => {
+	return (
+		<>
+			<Paper
+				sx={{
+					display: "flex",
+					flexDirection: "row",
+					justifyContent: "space-around",
+					width: "100%",
+					height: "700px",
+
+					backgroundColor: "#eeeeee",
+					marginTop: "100px",
+				}}
+			>
+				<Box
+					sx={{
+						display: "flex",
+						flexDirection: "column",
+						flex: 1,
+						alignContent: "space-between",
+						marginTop: "50px",
+						marginLeft: "40px",
+					}}
+				>
+					<Typography variant="h4">
+						Más de 10 años trabajando con los mejores precios, sin
+						intermediarios para una mejor experiencia de alquiler de yates.
+					</Typography>
+
+					<Typography variant="h6" marginTop={5}>
+						Somos propietarios de nuestra flota de yates, los cuales están en
+						perfecto estado de conservación y cuentan con todas las medidas de
+						seguiridad necesarias para que su alquiler sea una experiencia
+						inolvidable.
+					</Typography>
+					<Box sx={{ color: Colors.first_blue }} marginTop={5}>
+						<Box
+							sx={{
+								display: "flex",
+								flexDirection: "row",
+							}}
+						>
+							<Check />
+							<Typography variant="h5" marginLeft={1}>
+								Ahorra disgustos
+							</Typography>
+						</Box>
+						<Box
+							sx={{
+								display: "flex",
+								flexDirection: "row",
+							}}
+						>
+							<Check />
+							<Typography variant="h5" marginLeft={1}>
+								Contrata expertos
+							</Typography>
+						</Box>
+						<Box
+							sx={{
+								display: "flex",
+								flexDirection: "row",
+							}}
+						>
+							<Check />
+							<Typography variant="h5" marginLeft={1}>
+								Sin intermediarios
+							</Typography>
+						</Box>
+						<Box
+							sx={{
+								display: "flex",
+								flexDirection: "row",
+							}}
+						>
+							<Check />
+							<Typography variant="h5" marginLeft={1}>
+								Garantiza tu seguridad
+							</Typography>
+						</Box>
+						<Box
+							sx={{
+								display: "flex",
+								flexDirection: "row",
+							}}
+						>
+							<Check />
+							<Typography variant="h5" marginLeft={1}>
+								Vamos a navegar
+							</Typography>
+						</Box>
+					</Box>
+				</Box>
+				<Box
+					sx={{
+						flex: 1,
+						backgroundImage:
+							"url(https://res.cloudinary.com/freelancer2222222222222222/image/upload/v1671227427/DJI_0426_aisn2a.jpg)",
+						maxWidth: "100%",
+						height: "500px",
+						backgroundPosition: "50% 50%",
+						backgroundSize: "cover",
+						backgroundRepeat: "no-repeat",
+						clipPath:
+							"polygon(75% 0%, 100% 50%, 75% 100%, 0% 100%, 25% 50%, 0% 0%)",
+						marginTop: "100px",
+						marginLeft: "50px",
+					}}
+				></Box>
+			</Paper>
+		</>
+	);
+};
--- components/hero/DestinationHero.jsx
+++ components/hero/DestinationHero.jsx
@@ -1,34 +1,37 @@
 import { Paper, Box, Typography } from "@mui/material";
 
-
-
 export const DestinationHero = () => {
 	return (
 		<>
-           <Paper
-                sx={{
-                    backgroundImage: "url(https://res.cloudinary.com/freelancer2222222222222222/image/upload/v1671227462/DJI_0344_wxwfit.jpg)",
-                    width: "100%",
-                    maxWidth: "100%",
-                    height: "400px",
-                    backgroundPosition: "50% 50%",
-                    backgroundSize: "cover",
-                    backgroundRepeat: "no-repeat"
-                }}
-		        >
-                <Box sx={{
-                    width: "100%",
-                    height: "100%",
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    // position: "absolute",
-                    }}>
-                    <Typography  variant="h2">DESTINATIONS:</Typography>
-                    <Typography  variant="h4">Choose one of the destinations we have for you.</Typography>
-                </Box>
-            </Paper>
-        </>
-    )
-}
+			<Paper
+				sx={{
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+					backgroundImage:
+						"url(https://res.cloudinary.com/freelancer2222222222222222/image/upload/v1671227462/DJI_0344_wxwfit.jpg)",
+					maxWidth: "100%",
+					height: "400px",
+					backgroundPosition: "50% 50%",
+					backgroundSize: "cover",
+					backgroundRepeat: "no-repeat",
+				}}
+			>
+				<Box
+					sx={{
+						display: "flex",
+						flexDirection: "column",
+						color: "white",
+						alignItems: "center",
+					}}
+				>
+					<Typography variant="h4">DESTINATIONS:</Typography>
+
+					<Typography variant="h5">
+						Choose one of the destinations we have for you.
+					</Typography>
+				</Box>
+			</Paper>
+		</>
+	);
+};
