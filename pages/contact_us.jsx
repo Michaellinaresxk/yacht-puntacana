de12d4f18b64d8b2a941da2d12c5c9d5b808f99b
Michael Linares <116801315+Michaellinaresxk@users.noreply.github.com>
2022-12-17 01:08:12 +0100

    Create hero for contact page (#17)

--- pages/contact_us.jsx
+++ pages/contact_us.jsx
@@ -1,11 +1,11 @@
 import DrawerAppBar from "../components/Header/DrawerAppBar";
-import { Typography } from "@mui/material";
+import { ContactHero } from "../components/hero/ContactHero";
 
 function contact_us() {
 	return (
 		<>
 			<DrawerAppBar />
-			<Typography variant="h2">This is the contact page</Typography>
+			<ContactHero />
 		</>
 	);
 }
