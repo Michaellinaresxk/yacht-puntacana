daf1eeb745a85eba86ffdc165293cec407d1c1ed
AJMS1118 <ajquesadac90@gmail.com>
2022-12-22 17:42:21 -0500

    added video component and make it responsive

--- components/VideoComponent.jsx
+++ components/VideoComponent.jsx
@@ -1,7 +1,40 @@
+import { Card, CardMedia, Paper } from "@mui/material";
+
 export const VideoComponent = () => {
 	return (
 		<>
-			<div>Video Component</div>
+			<Paper
+				sx={{
+					width: "80%",
+					margin: "auto",
+					marginTop: "50px",
+					marginBottom: "50px",
+					backgroundColor: "aqua",
+				}}
+				elevation={10}
+			>
+				<Card sx={{}}>
+					<CardMedia
+						component="iframe"
+						allowFullScreen={true}
+						sx={{
+							width: {
+								sx: "90%",
+								md: "85%",
+								lg: "80%",
+							},
+							margin: "auto",
+							height: {
+								xs: "150px",
+								sm: "300px",
+								md: "350px",
+								lg: "500px",
+							},
+						}}
+						image="https://res.cloudinary.com/freelancer2222222222222222/video/upload/v1671285248/video-tour_sxqsw8.mp4"
+					/>
+				</Card>
+			</Paper>
 		</>
 	);
 };
