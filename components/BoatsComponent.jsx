c757c12ccbe6b68309c553076a97d529c77ff414
AJMS1118 <ajquesadac90@gmail.com>
2022-12-23 09:05:42 -0500

    adding horizontal margin to the card of boats in mobile view

--- components/BoatsComponent.jsx
+++ components/BoatsComponent.jsx
@@ -24,10 +24,10 @@ export const BoatsComponent = ({ props }) => {
 			>
 				<Grid container spacing={3} rowGap={2} columnGap={0}>
 					{boats.map((boat) => (
-						<Grid item key={boat.id} sx={12} sm={6} md={4}>
+						<Grid item key={boat.id} xs={12} sm={6} md={4}>
 							<Paper
 								sx={{
-									maxWidth: { xs: "90%" },
+									width: { xs: "80%" },
 									marginLeft: { xs: "auto" },
 									marginRight: { xs: "auto" },
 								}}
