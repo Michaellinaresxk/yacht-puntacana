89b3672816eeb4c57bfdc608e24c016e337f1e85
Michael Linares <116801315+Michaellinaresxk@users.noreply.github.com>
2022-12-23 22:33:18 +0100

    Added global components to services page and contact page (#32)

--- pages/contact_us.jsx
+++ pages/contact_us.jsx
@@ -1,11 +1,13 @@
 import { ContactHero } from "../components/hero/ContactHero";
 import { ContactGroupIcons } from "../components/icons/ContactGroupIcons";
+import { Footer } from "../components/Footer";
 
 function contact_us() {
 	return (
 		<>
 			<ContactHero />
 			<ContactGroupIcons />
+			<Footer />
 		</>
 	);
 }
