93ce864796165f6f8f6919555394bd0b446e56f8
Andrés de Jesús Quesada Cedeño <92536363+AJMS1118@users.noreply.github.com>
2022-12-28 18:01:12 -0500

    adding the boats descrption and the drawer routing (#40)
    Co-authored-by: AJMS1118 <ajquesadac90@gmail.com>

--- components/DescriptionBoatComponent.jsx
+++ components/DescriptionBoatComponent.jsx
@@ -4,13 +4,25 @@ export const DescriptionBoatComponent = ({ data }) => {
 	return (
 		<Box
 			sx={{
-				marginTop: "20px",
-				border: "1px solid #212121",
+				marginTop: "50px",
+				width: "auto",
+				maxWidth: "90%",
+				marginLeft: "auto",
+				marginRight: "auto",
 			}}
 		>
 			<Typography variant="h4">Description</Typography>
-			<Typography variant="subtitle1" mt={5}>
-				zvnjvnjkcx vcjvnjcvkjc vcjvnjnvjkxc
+
+			<Typography variant="subtitle1" mt={2} component="p">
+				{data.p1}
+			</Typography>
+
+			<Typography variant="subtitle1" mt={2} component="p">
+				{data.p2}
+			</Typography>
+
+			<Typography variant="subtitle1" mt={2} component="p">
+				{data.p3}
 			</Typography>
 		</Box>
 	);
