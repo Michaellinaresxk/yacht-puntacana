181af3fb0233653b74279e6445c5b7453c1e104e
AJMS1118 <ajquesadac90@gmail.com>
2022-12-30 01:28:19 -0500

    adding form component

--- pages/contact_us.jsx
+++ pages/contact_us.jsx
@@ -1,12 +1,14 @@
 import { ContactHero } from "../components/hero/ContactHero";
 import { ContactGroupIcons } from "../components/icons/ContactGroupIcons";
 import { Footer } from "../components/Footer";
+import { FormComponent } from "../components/FormComponent";
 
 function contact_us() {
 	return (
 		<>
 			<ContactHero />
 			<ContactGroupIcons />
+			<FormComponent />
 			<Footer />
 		</>
 	);
