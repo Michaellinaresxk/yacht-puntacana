c0a1a6f4a49c6667379518088faec1b3a55e3ed5
AJMS1118 <ajquesadac90@gmail.com>
2023-01-08 23:48:09 -0500

    marker added

--- components/map/NewMap.jsx
+++ components/map/NewMap.jsx
@@ -1,6 +1,7 @@
-import { Map, NavigationControl } from "react-map-gl";
+import { Map, NavigationControl, Marker } from "react-map-gl";
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
+import { Colors } from "../../styles/theme";
 
 export const NewMap = () => {
 	return (
@@ -21,6 +22,11 @@ export const NewMap = () => {
 				mapStyle="https://api.maptiler.com/maps/streets/style.json?key=RD2wTsj4fy4oPde6tlVb"
 			>
 				<NavigationControl position="top-left" />
+				<Marker
+					longitude={-68.90117}
+					latitude={18.403121}
+					color={Colors.first_blue}
+				/>
 			</Map>
 		</>
 	);
