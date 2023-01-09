4e34f62d8e084c9cf37861f54551b1f15a18cb32
Andrés de Jesús Quesada Cedeño <92536363+AJMS1118@users.noreply.github.com>
2023-01-09 09:25:40 -0500

    deleting things (#56)
    Co-authored-by: AJMS1118 <ajquesadac90@gmail.com>

--- pages/contact_us.jsx
+++ pages/contact_us.jsx
@@ -2,15 +2,15 @@ import { ContactHero } from "../components/hero/ContactHero";
 import { ContactGroupIcons } from "../components/icons/ContactGroupIcons";
 import { Footer } from "../components/Footer";
 import { FormComponent } from "../components/FormComponent";
-import TestCard from "../components/cards/TestCard";
+import { NewMap } from "../components/map/NewMap";
 
 function contact_us() {
 	return (
 		<>
 			<ContactHero />
 			<ContactGroupIcons />
+			<NewMap />
 			<FormComponent />
-			<TestCard />
 			<Footer />
 		</>
 	);
